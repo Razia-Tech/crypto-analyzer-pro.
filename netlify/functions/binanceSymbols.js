ad9c30094c4b8bfc3b40618900e4861667c4a3b4
Razia Tech <kaiosiddik@gmail.com>
2025-08-15 21:13:06 +0700

    Update binanceSymbols.js

--- netlify/functions/binanceSymbols.js
+++ netlify/functions/binanceSymbols.js
@@ -2,7 +2,7 @@ export async function handler() {
   try {
     const res = await fetch("https://api.binance.com/api/v3/exchangeInfo", {
       headers: {
-        "X-MBX-APIKEY": process.env.BINANCE_API_KEY // Set di Netlify
+        "X-MBX-APIKEY": process.env.BINANCE_API_KEY
       }
     });
 
